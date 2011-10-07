9f2fc5985993a1ae94066618d3ecb7f28298b6bb
chenry <chenry@mcs.anl.gov>
2011-10-07 14:24:23 -0500

    d

--- lib/ModelSEED/ModelSEEDScripts/configureMFAToolkit.pl
+++ lib/ModelSEED/ModelSEEDScripts/configureMFAToolkit.pl
@@ -42,7 +42,7 @@ if ($os eq "windows") {
 } else {
 	#Running make
 	File::Copy::copy($args->{"-p"}."software/mfatoolkit/Linux/makeMFAToolkit.sh",$args->{"-p"}."software/mfatoolkit/bin/makeMFAToolkit.sh");
-	chmod 0775,$directoryRoot."/software/mfatoolkit/bin/makeMFAToolkit.sh";
+	chmod 0775,$args->{"-p"}."/software/mfatoolkit/bin/makeMFAToolkit.sh";
 	my $makeCommand = $args->{"-p"}."software/mfatoolkit/bin/makeMFAToolkit.sh";
 	if (defined($args->{"-clean"})) {
 		$makeCommand .= " clean"; 
