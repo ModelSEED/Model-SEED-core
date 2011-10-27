a2ac0e1c9d8a0494724fd84c58c0b6d20f798f76
seaver <samseaver@gmail.com>
2011-10-27 13:57:39 -0500

    commiting changes in ModelSEED/FIGMODEL.pm

--- lib/ModelSEED/FIGMODEL.pm
+++ lib/ModelSEED/FIGMODEL.pm
@@ -1586,9 +1586,9 @@ sub parseSBMLtoTable {
 	    $args->{error}="Value for directory parameter (".$args->{directory}.") is not a directory\n";
 	}
 	
-	
 	my $dir = $args->{directory};
 	my $name = $args->{model_name};
+	$name = "ModelData" if !$name;
 	$name.="-";
 	
 	$args = $self->process_arguments($args,["directory"],{
@@ -1605,8 +1605,9 @@ sub parseSBMLtoTable {
     }elsif(defined($args->{file})){
 	my $dir=substr($args->{file},0,rindex($args->{file},'/')+1);
 	my $name = $args->{model_name};
+	$name = substr($args->{file},rindex($args->{file},'/')+1,rindex($args->{file},'.')-rindex($args->{file},'/')-1) if !$name;
 	$name.="-";
-	
+
 	$args = $self->process_arguments($args,["file"],{
 	    SBMLFile => $args->{file},
 	    compartmentFiles => $dir.$name."compartments.tbl",
@@ -2689,15 +2690,16 @@ sub import_model {
 		});
 	} elsif ($args->{overwrite} == 0) {
 		return $self->new_error_message({message=> $id." already exists and overwrite request was not provided. Import halted.".$args->{owner},function => "import_model",args => $args});
-	} elsif (defined($args->{biochemSource})){
-		$modelObj->GenerateModelProvenance({
-		    biochemSource => $args->{biochemSource}
-		});
 	}
 
-	$mdl = $self->get_model($id);
+        $mdl = $self->get_model($id);
+
+        if ($args->{overwrite} == 1 && defined($args->{biochemSource})){
+            $mdl->GenerateModelProvenance({biochemSource => $args->{biochemSource}});
+        }
 
 	my $importTables = ["reaction","compound","cpdals","rxnals"];
+	my %CompoundAlias=();
 	if (defined($id) && length($id) > 0 && defined($mdl)) {
 		for (my $i=0; $i < @{$importTables}; $i++) {
 			$mdl->figmodel()->database()->freezeFileSyncing($importTables->[$i]);
@@ -2750,8 +2752,8 @@ sub import_model {
 
 		my $cpdals = $mdl->figmodel()->database()->get_object("cpdals",{alias => $row->{"ID"}->[0],type => "BKM"});
 		if (defined($cpdals)) {
-		    print "Found using InChIs: ",$cpd->id()," for id ",$row->{ID}->[0],"\n";
 		    $cpd =  $mdl->figmodel()->database()->get_object("compound",{id => $cpdals->COMPOUND()});
+		    print "Found using InChIs: ",$cpd->id()," for id ",$row->{ID}->[0],"\n";
 		}
 
 		my $newNames;
@@ -2761,7 +2763,6 @@ sub import_model {
 				for (my $k=0; $k < @{$searchNames}; $k++) {
 				    #Look for searchname in original 'searchname' type
 				    my $cpdals = $mdl->figmodel()->database()->get_object("cpdals",{alias => $searchNames->[$k],type => "searchname"});
-
 				  
 				    #if not, look for it in previous imports
 				    if (!defined($cpdals)) {
@@ -2776,8 +2777,18 @@ sub import_model {
 					    $cpd = 	$mdl->figmodel()->database()->get_object("compound",{id => $cpdals->COMPOUND()});
 					}
 				    } else {
-					$newNames->{name}->{$row->{"NAMES"}->[$j]} = 1;
+					#prevent use of names that being with cpd, for obvious confusion
+					#with ModelSEED identifiers
+					next if substr($searchNames->[$k],0,3) eq "cpd";
+
+					#stopgap to prevent names being inserted that are too close
+					#this occurs because the database doesn't recognize upper/lower case when
+					#using indexes
+					if(!exists($newNames->{search}->{$searchNames->[$k]})){
+					    $newNames->{name}->{$row->{"NAMES"}->[$j]} = 1;
+					}
 					$newNames->{search}->{$searchNames->[$k]} = 1;
+
 				    }
 				}
 			}
