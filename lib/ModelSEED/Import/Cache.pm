ed6da83e34fa5c10e6a11b49902a8df57f7c3958
Scott Devoid <sdevoid@gmail.com>
2012-02-27 16:24:05 -0600

    Cleanup Import Code

--- lib/ModelSEED/Import/Cache.pm
+++ lib/ModelSEED/Import/Cache.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Moose;
 
-has 'om' => ( is => 'rw', required => 1, isa => 'ModelSEED::ObjectManager');
-has 'importer' => ( is => 'rw', isa => 'ModelSEED::Import', required => 1);
+has 'om' => (is => 'rw', required => 1, isa => 'ModelSEED::ObjectManager');
+has 'importer' => (is => 'rw', isa => 'ModelSEED::Import', required => 1);
 
 1;
--- lib/ModelSEED/Import/Checker.pm
+++ lib/ModelSEED/Import/Checker.pm
@@ -20,46 +20,46 @@ with 'MooseX::Log::Log4perl';
 #   are expected to fail given a priori knowledge of how
 #   the importer is working and what objects are problematic.
 #   This is represented as a hashref with the following structure:
-#    
+#
 #    {
 #        reaction => [ 'rxn12345', rxn12346', ...],
 #        compound => [ 'cpd00001', ... ],
 #    }
 #
 
-has 'expectedFailure' => ( is => 'rw', isa => 'HashRef', default => sub { return {}; });
-has 'efFile' => ( is => 'rw', isa => 'Str');
+has 'expectedFailure' =>
+    (is => 'rw', isa => 'HashRef', default => sub { return {}; });
+has 'efFile' => (is => 'rw', isa => 'Str');
 
 sub BUILD {
     my ($self) = @_;
-    if(defined($self->efFile) && -f abs_path($self->efFile)) {
+    if (defined($self->efFile) && -f abs_path($self->efFile)) {
         $self->load_ef_from_file(abs_path($self->efFile));
     }
 }
 
-# explanation 
+# explanation
 sub check {
     my ($self, $header, $time, $missed) = @_;
     $self->logger->info("$header in $time seconds\n");
     $self->logger->info("==== Begin Errors ====\n");
     foreach my $type (keys %$missed) {
-        if(ref($missed->{$type}) eq 'ARRAY') {
+        if (ref($missed->{$type}) eq 'ARRAY') {
             map { $self->checkObject($type, $_) } @{$missed->{$type}};
         } else {
-            while( my ($id, $explanation) =
-                each %{$missed->{$type}} ) {
+            while (my ($id, $explanation) = each %{$missed->{$type}}) {
                 $self->checkObject($type, $id, $explanation);
             }
         }
     }
     $self->logger->info("==== End Errors   ====\n");
-    
+
 }
 
 sub checkObject {
     my ($self, $type, $id, $explanation) = @_;
     my $expected = $self->expectedFailure->{$type};
-    if(defined($expected) && $id ~~ @$expected) {
+    if (defined($expected) && $id ~~ @$expected) {
         return;
     } else {
         $self->warnObject($type, $id, $explanation);
@@ -70,12 +70,13 @@ sub warnObject {
     my ($self, $type, $id, $explanation) = @_;
     $explanation = "\n\t$explanation" if defined $explanation;
     $explanation = "" unless defined $explanation;
-    $self->logger->info("Failed to create object [$type]: $id;\n$explanation");
+    $self->logger->info(
+        "Failed to create object [$type]: $id;\n$explanation");
 }
 
 sub load_ef_from_file {
     my ($self, $efFile) = @_;
-    my $j = JSON::Any->new();
+    my $j    = JSON::Any->new();
     my $text = File::Slurp::read_file($efFile);
     $self->expectedFailure($j->jsonToObj($text));
 }
