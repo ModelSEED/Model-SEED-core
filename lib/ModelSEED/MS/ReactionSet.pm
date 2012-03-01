15a41e5e0b7d4d9988bdc1a505b0f307d8bc7914
Paul Frybarger <pfrybarger@gmail.com>
2012-03-01 14:43:08 -0600

    Renamed Reactionset.pm and Compoundset.pm to ReactionSet.pm and CompoundSet.pm.
    Initial work on Model.pm, modeled after Biochemistry.pm.

---
+++ lib/ModelSEED/MS/ReactionSet.pm
@@ -0,0 +1,46 @@
+package ModelSEED::MS::ReactionSet;
+use Moose;
+use Moose::Util::TypeConstraints;
+use namespace::autoclean;
+use Data::UUID;
+use DateTime;
+
+# Attributes
+has 'uuid' => ( is => 'rw', isa => 'Str', builder => '_buildUUID' );
+has 'modDate' => ( is => 'rw', isa => 'Str', builder => '_buildModDate' );
+has 'locked' => ( is => 'rw', isa => 'Int', default => 0 );
+has 'name' => ( is => 'rw', isa => 'Str' );
+has 'searchname' => ( is => 'rw', isa => 'Str' );
+has 'class' => ( is => 'rw', isa => 'Str' );
+has 'type' => ( is => 'rw', isa => 'Str' );
+# Relationships
+has 'reactions' => ( is => 'rw', default => sub { return []; },
+	isa => 'ArrayRef[ModelSEED::MS::Reaction]');
+
+sub BUILDARGS {
+    my ($self, $params) = @_;
+    my $attrs = $params->{attributes};
+    my $rels  = $params->{relations} || {};
+    if(defined($attrs)) {
+        map { $params->{$_} = $attrs->{$_} } @$attrs;
+        delete $params->{attributes};
+    }
+    my $bio = $params->{biochemistry}; 
+    delete $params->{biochemistry};
+    # pretty ugly
+    foreach my $rel (keys %$rels) {
+        if($rel eq 'reactions') {
+            map { $_ = $bio->getReaction(
+            { uuid => $_ }) } @{$rels->{$rel}};
+        }
+        $params->{$rel} = $rels->{$rel};
+    }
+    delete $params->{relations};
+    return $params;
+}
+
+sub _buildUUID { return Data::UUID->new()->create_str(); }
+sub _buildModDate { return DateTime->now(); }
+
+__PACKAGE__->meta->make_immutable;
+1;
