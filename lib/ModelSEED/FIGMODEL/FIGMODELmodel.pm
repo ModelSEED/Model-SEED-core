76cd4be228b8af0eb7ce9b9cd1ce9c659eaf61cd
seaver <seaver@mcs.anl.gov>
2012-03-04 00:01:05 -0600

    Fixed return string bug in mdl-changedrains, works fine

--- lib/ModelSEED/FIGMODEL/FIGMODELmodel.pm
+++ lib/ModelSEED/FIGMODEL/FIGMODELmodel.pm
@@ -359,10 +359,12 @@ sub changeDrains {
 			$drnHash->{$args->{drains}->[$i]}->{min} = -10000;
 		}
 	}
-	my $drainString = "cpd11416[c]:-10000:0;cpd15302[c]:-10000:10000;cpd08636[c]:-10000:0";
+	my $drainString = "";
 	foreach my $drn (keys(%{$drnHash})) {
 		$drainString .= ";".$drn.":".$drnHash->{$drn}->{min}.":".$drnHash->{$drn}->{max};
 	}
+	
+
 	if ($drainString ne "cpd11416[c]:-10000:0;cpd15302[c]:-10000:10000;cpd08636[c]:-10000:0") {
 		ModelSEED::utilities::PRINTFILE($self->figmodel()->config('model directory')->[0].$self->owner()."/".$self->id()."/drains.txt",[$drainString]);
 	}
