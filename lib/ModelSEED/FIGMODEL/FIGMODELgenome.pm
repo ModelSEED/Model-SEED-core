e6f58f1cf13d809f4f303d7a59b6b9ede02ed920
chenry <chenry@mcs.anl.gov>
2012-03-22 17:42:32 -0500

    d

--- lib/ModelSEED/FIGMODEL/FIGMODELgenome.pm
+++ lib/ModelSEED/FIGMODEL/FIGMODELgenome.pm
@@ -356,8 +356,8 @@ sub update_genome_stats {
 	my $GenomeData;
 	my $sap = $self->figmodel()->sapSvr("PUBSEED");
 	my $result = $sap->exists({-type => 'Genome',-ids => [$self->genome()]});
-	print $result->{$self->genome()}."\n";
 	if (!defined($result->{$self->genome()}) || $result->{$self->genome()} == 0) {
+		$genomeStats->{source} = "RAST";
 		my $output = $self->getRastGenomeData();
 		if (!defined($self->{_features})) {
 			$self->error_message("FIGMODELgenome:genome_stats:Could not find genome ".$self->genome()." in database");
