93ee7bbfb05515b86d80118da04bc3dd6de544ba
Scott Devoid <sdevoid@gmail.com>
2012-05-01 12:05:09 -0500

    Adding documetation for Store, Store::Private
    - Added POD documentation

--- lib/ModelSEED/Store.pm
+++ lib/ModelSEED/Store.pm
@@ -1,3 +1,78 @@
+########################################################################
+# ModelSEED::Store - Authenticated storage interface layer
+# Authors: Christopher Henry, Scott Devoid, Paul Frybarger
+# Contact email: chenry@mcs.anl.gov
+# Development location: 
+#   Mathematics and Computer Science Division, Argonne National Lab;
+#   Computation Institute, University of Chicago
+#                       
+# Date of module creation: 2012-05-01
+########################################################################
+=pod
+
+=head1 NAME
+
+ModelSEED::Store - Authenticated storage interface layer
+
+=head1 METHODS
+
+=head2 new
+
+    my $Store = ModelSEED::Store->new(\%);
+
+This initializes a Storage interface object. This accepts a hash
+reference to configuration details. In addition to authentication,
+which is required and will be covered in a moment, this currently
+accepts one parameter:
+
+=over
+
+=item C<private>
+
+A reference to a C<ModelSEED::Store::Private> object. This is the
+base storage interface that the Store will use. If this is not
+provided, it will be initialized based on the contents of the
+C<ModelSEED::Configuration> package.
+
+=head3 Authentication
+
+The C<new> method requires authentication information. Without this,
+or if authentication fails, the class will raise an exception
+C<"Unauthorized">. Currently the class accepts one type of
+authentication:
+
+=over
+
+=item Basic
+
+Basic Authentication - Requires "username" and "password" fields.
+This will attempt to authenticate against the data in the Store::Private
+dataset under the type "user". If no user is found, it will attempt
+to use the C<ModelSEED::ModelSEEDClients::MSSeedSupportClient>
+package to retrieve authentication information from the central
+servers.
+
+=back
+
+=head2 Other Functions
+
+This package has the same set of functions as C<ModelSEED::Store::Private>
+but these functions do not accept a "user" as the first argument.
+All other arguments are "shifted up by one". For example, in the
+private interface, we have this function:
+
+    my $data = $StorePrivate->get_data("alice", "model", "alice/myModel");
+
+This is equivalent to the following function in the C<ModelSEED::Store>
+interface, provided that the user C<alice> authenticated during
+initialization:
+
+    my $data = $Store->get_data("model", "alice/myModel");
+
+Therefore, you should consult the C<ModelSEED::Store::Private>
+documentation for further method details.
+
+=cut
 package ModelSEED::Store;
 use Moose;
 use ModelSEED::Store::Private;
@@ -32,8 +107,7 @@ around BUILDARGS => sub {
                         lastname => $info->{lastname},
                         email => $info->{email},
                     });
-                $private->create_user("user", $user->login,
-                    $user->serializeToDB);
+                $private->create_user($user->login, $user->serializeToDB);
             }
         }
         if($user->check_password($args->{password})) {
--- lib/ModelSEED/Store/Private.pm
+++ lib/ModelSEED/Store/Private.pm
@@ -1,3 +1,170 @@
+########################################################################
+# ModelSEED::Store::Private - Base storage interface layer
+# Authors: Christopher Henry, Scott Devoid, Paul Frybarger
+# Contact email: chenry@mcs.anl.gov
+# Development location: 
+#   Mathematics and Computer Science Division, Argonne National Lab;
+#   Computation Institute, University of Chicago
+#                       
+# Date of module creation: 2012-05-01
+########################################################################
+=pod
+
+=head1 NAME
+
+ModelSEED::Store - Base storage interface layer
+
+=head1 NOTE
+
+DO NOT USE Directly! This class has a complete, unauthenticated
+access to the datastore. While the datastore is designed to limit
+the visibility of objects to particular users, this class does
+nothing to prevent people from accessing data. The "user" is passed
+into each API function, therefore there is no assumption of security
+when using this interface.  Use C<ModelSEED::Store> instead.
+
+=head1 Initialization
+
+=head2 new
+
+    my $PStore = ModelSEED::Store::Private->new(\%)
+
+Initialize a private storage interface object. This accepts a hash
+reference to configuration details. There are no current configuration
+options.
+
+=head1 User Functions
+
+These functions manipulate user objects within the storage layer.
+Since a user must be defined for almost every call into the storage
+interface, these functions are critical to add, remove or inspect
+user objects.
+
+=head2 create_user
+
+    $PStore->create_user($username, \%);
+
+Creates a user object. C<$username> is a unique string representing
+the user's login name. C<\%> is a hashref containing user information.
+This should really be the contents of C<ModelSEED::MS::User>.
+
+
+=head2 get_user
+
+    $PStore->get_user($username);
+
+Returns a C<ModelSEED::MS::User> object. If no user is found, returns
+C<undef>.
+
+=head2 delete_user
+
+    $PStore->delete_user($username);
+
+Removes the user object from the storage interface. Not that this
+has wide-ranging affects; that user will no longer be able to access
+objects within the Store. This does not delete objects owned by that
+user, however.
+
+=head1 Object Methods 
+
+These functions deal with checking for the existence of, getting
+and saving objects. In most cases, objects are assumed to be instances
+of classes under the C<ModelSEED::MS> hierarchy. For each of these
+functions, C<$username> is the user's login name, a string. C<$type>
+is a string representing the type of the object, e.g. "biochemistry".
+C<$alias> is a string, representing a unique pointer to an object
+in the datastore.  These generally have the form
+C<"$username/arbitraryString">.
+
+
+=head2 has_object
+
+    $ps->has_object($username, $type, $alias);
+
+Returns a boolean, true if the the object exists in the datastore.
+
+=head2 get_object
+
+    $ps->get_object($username, $type, $alias);
+
+Returns a C<ModelSEED::MS> object of the type C<$type> or
+C<undef> if no object exists.
+
+=head2 save_object
+
+    $ps->save_object($username, $type, $alias, $object);
+
+Saves C<$object>, a C<ModelSEED::MS> object of the correct C<$type>
+to the datastore at C<$alias>. Note that if there was an object
+already at C<$alias>, this does not overwrite that object, but
+changes the reference C<$alias> to point to the new object, C<$object>.
+
+=head2 delete_object
+
+    $ps->delete_object($username, $type, $alias);
+
+Removes the alias pointer C<$alias> that points to an object in
+the datastore. This does not actually remove the object, but it will
+no longer be accessible via that alias.
+
+=head1 Data Methods
+
+These methods are like the Object Methods, except that they
+return raw Perl data-structures instead of C<ModelSEED::MS> objects.
+
+
+=head2 get_data
+
+    $ps->get_data($username, $type, $alias);
+
+Same as C<get_object> except that it is not marshaled into an object.
+
+
+=head1 Querying
+
+Getting a specific object is good, but sometimes we need to query what
+objects are available. These functions allow for different types of
+queries against the data store.
+
+=head2 get_aliases_for_type
+
+    $ps->get_aliases_for_type($username, $type);
+
+=head1 Permissions: Editing and Viewing
+
+Permissions are handled via the "alias" attribute and the following
+functions. As an overview, any object with an alias like
+C<"$username/aribtraryString> is "owned" by C<$username>. That
+user may perform a C<save_object> call against the alias. No other
+users may do this. So we have single-user write-access to objects.
+
+For visibility, by default an object is only visible by the owner.
+However, using the following functions, an owner may extend visibility
+to other users. Finally, an object may be "public", visible to all
+users.
+
+=head2 add_viewer
+
+    $ps->add_viewer($username, $type, $alias, $viewerUsername);
+
+C<$username> must be the owner of the object of C<$type>, C<$alias>.
+This extends viewing permissions to C<$viewerUsername>.
+
+=head2 remove_viewer
+
+    $ps->remove_viewer($username, $type, $alias, $viewerUsername);
+
+C<$username> must be the owner of the object of C<$type>, C<$alias>.
+This retracts viewing permissions to C<$viewerUsername>.
+
+=head2 set_public
+
+    $ps->set_public($username, $type, $alias, $bool);
+
+C<$username> must be the owner of the object of C<$type>, C<$alias>.
+This sets the public bit of the object to C<$bool>.
+
+=cut
 package ModelSEED::Store::Private;
 use Moose;
 use Try::Tiny;
@@ -42,7 +209,7 @@ sub create_user {
     my ($self, $user, $object) = @_;
 
     unless ($self->db->save_object('user', $user, $object)) {
-	return 0;
+    return 0;
     }
 
     # now save type meta
@@ -65,11 +232,6 @@ sub delete_user {
     return $self->db->delete_object('user', $user);
 }
 
-=head
-    is this method needed?
-
-    has_object(user, type, alias);
-=cut
 sub has_object {
     my ($self, $user, $type, $user_alias) = @_;
 
@@ -82,23 +244,20 @@ sub has_object {
     my $meta_path = "$RESERVED_META.aliases.$alias";
     my $info = $self->db->get_metadata('user', $alias_user, $meta_path);
     unless (defined($info)) {
-	# no object, return false
-	return 0;
+    # no object, return false
+    return 0;
     }
 
     # check permissions
     unless ($user eq $alias_user ||
-	    $info->{public}      ||
-	    $info->{viewers}->{$user}) {
-	return 0;
+        $info->{public}      ||
+        $info->{viewers}->{$user}) {
+    return 0;
     }
 
     return 1;
 }
 
-=head
-    get_object(user, type, alias);
-=cut
 sub get_object {
     my ($self, $user, $type, $user_alias) = @_;
     $type = lc($type);
@@ -108,9 +267,6 @@ sub get_object {
     return $class->new($obj_data);
 }
 
-=head
-    get_data(user, type, alias);
-=cut
 sub get_data {
     my ($self, $user, $type, $user_alias) = @_;
 
@@ -123,24 +279,21 @@ sub get_data {
     my $meta_path = "$RESERVED_META.aliases.$alias";
     my $info = $self->db->get_metadata('user', $alias_user, $meta_path);
     unless (defined($info)) {
-	# no object, return undef
-	return;
+    # no object, return undef
+    return;
     }
 
     # check permissions
     unless ($user eq $alias_user ||
-	    $info->{public}      ||
-	    $info->{viewers}->{$user}) {
-	return;
+        $info->{public}      ||
+        $info->{viewers}->{$user}) {
+    return;
     }
 
     my $obj_id = $info->{object};
     return $self->db->get_object($type, $obj_id);
 }
 
-=head
-    save_object(user, type, alias, object);
-=cut
 sub save_object {
     my ($self, $user, $type, $user_alias, $object) = @_;
 
@@ -151,8 +304,8 @@ sub save_object {
     # split user from alias (paul/main)
     my ($alias_user, $alias) = _split_alias($user_alias);
     unless ($user eq $alias_user) {
-	# cannot save to someone else's alias space
-	return 0;
+    # cannot save to someone else's alias space
+    return 0;
     }
 
     # save data, unless object already exists (check via md5)
@@ -160,46 +313,43 @@ sub save_object {
     my $md5 = md5_hex($json_obj);
     my $new = 0;
     unless ($self->db->has_object($type, $md5)) {
-	$new = 1;
-	$self->db->save_object($type, $md5, $json_obj);
+    $new = 1;
+    $self->db->save_object($type, $md5, $json_obj);
 
-	# now save type meta
-	my $obj_meta = {
-	    type    => $type,
-	    parents => []
-	};
+    # now save type meta
+    my $obj_meta = {
+        type    => $type,
+        parents => []
+    };
 
-	my $meta_path = "$RESERVED_META";
-	$self->db->set_metadata($type, $md5, $meta_path, $obj_meta);
+    my $meta_path = "$RESERVED_META";
+    $self->db->set_metadata($type, $md5, $meta_path, $obj_meta);
     }
 
     # now save the alias
     my $meta_path = "$RESERVED_META.aliases.$alias";
     my $info = $self->db->get_metadata('user', $user, $meta_path);
     if (defined($info)) {
-	# update alias to point to new object
-	$self->db->set_metadata('user', $user, "$meta_path.object", $md5);
+    # update alias to point to new object
+    $self->db->set_metadata('user', $user, "$meta_path.object", $md5);
 
-	# get current list of parents and add parent obj
-	my $parents = $self->db->get_metadata($type, $md5, "$RESERVED_META.parents");
-	push(@$parents, $info->{object});
-	$self->db->set_metadata($type, $md5, "$RESERVED_META.parents", $parents);
+    # get current list of parents and add parent obj
+    my $parents = $self->db->get_metadata($type, $md5, "$RESERVED_META.parents");
+    push(@$parents, $info->{object});
+    $self->db->set_metadata($type, $md5, "$RESERVED_META.parents", $parents);
     } else {
-	# create new alias and permissions
-	$self->db->set_metadata('user', $user, $meta_path, {
-	    object  => $md5,
-	    type    => $type,
-	    public  => 0,
-	    viewers => {}
-	});
+    # create new alias and permissions
+    $self->db->set_metadata('user', $user, $meta_path, {
+        object  => $md5,
+        type    => $type,
+        public  => 0,
+        viewers => {}
+    });
     }
 
     return 1;
 }
 
-=head
-    delete_object(user, type, alias);
-=cut
 sub delete_object {
     my ($self, $user, $type, $user_alias) = @_;
 
@@ -208,17 +358,14 @@ sub delete_object {
     # split user from alias (paul/main)
     my ($alias_user, $alias) = _split_alias($user_alias);
     unless ($user eq $alias_user) {
-	# cannot delete from someone else's alias space
-	return 0;
+    # cannot delete from someone else's alias space
+    return 0;
     }
 
     my $meta_path = "$RESERVED_META.aliases.$alias";
     $self->db->remove_metadata($type, $user, $meta_path);
 }
 
-=head
-    get_aliases_for_type(user, type);
-=cut
 sub get_aliases_for_type {
     my ($self, $user, $type) = @_;
 
@@ -230,9 +377,9 @@ sub get_aliases_for_type {
 
     my $aliases = [];
     foreach my $alias (keys %$alias_hash) {
-	if ($alias_hash->{$alias}->{type} eq $type) {
-	    push(@$aliases, $alias);
-	}
+    if ($alias_hash->{$alias}->{type} eq $type) {
+        push(@$aliases, $alias);
+    }
     }
 
     return $aliases;
@@ -246,8 +393,8 @@ sub add_viewer {
     # split user from alias (paul/main)
     my ($alias_user, $alias) = _split_alias($user_alias);
     unless ($user eq $alias_user) {
-	# can only change viewers if you own the object
-	return 0;
+    # can only change viewers if you own the object
+    return 0;
     }
 
     my $meta_path = "$RESERVED_META.aliases.$alias.viewers.$viewer";
@@ -262,8 +409,8 @@ sub remove_viewer {
     # split user from alias (paul/main)
     my ($alias_user, $alias) = _split_alias($user_alias);
     unless ($user eq $alias_user) {
-	# can only change viewers if you own the object
-	return 0;
+    # can only change viewers if you own the object
+    return 0;
     }
 
     my $meta_path = "$RESERVED_META.aliases.$alias.viewers.$viewer";
@@ -274,7 +421,7 @@ sub set_public {
     my ($self, $user, $type, $user_alias, $public) = @_;
 
     unless ($public == 0 || $public == 1) {
-	return 0;
+    return 0;
     }
 
     $type = lc($type);
@@ -282,8 +429,8 @@ sub set_public {
     # split user from alias (paul/main)
     my ($alias_user, $alias) = _split_alias($user_alias);
     unless ($user eq $alias_user) {
-	# can only change viewers if you own the object
-	return 0;
+    # can only change viewers if you own the object
+    return 0;
     }
 
     my $meta_path = "$RESERVED_META.aliases.$alias.public";
