9baa1af737d2beb221cb97928605407e20ae2432
chenry <chenry@mcs.anl.gov>
2012-06-13 23:43:47 -0500

    Fixing bug in FBAFormulation

--- lib/ModelSEED/MS/FBAFormulation.pm
+++ lib/ModelSEED/MS/FBAFormulation.pm
@@ -44,8 +44,6 @@ sub runFBA {
 	$prob->printLPfile();
 	my $solution = $prob->submitLPFile();
 	my $readable = $solution->createReadableStringArray();
-	my $directory = "C:/Code/Model-SEED-core/data/exampleObjects/";
-	ModelSEED::utilities::PRINTFILE($directory."FBASolution.readable",$readable);
 	my $results = $self->add("fbaResults",{
 		name => $self->name()." results",
 		fbaformulation_uuid => $self->uuid(),
