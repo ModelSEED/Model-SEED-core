6b679583cb16c52a7f12a6b995ebd86d71ca09db
Scott Devoid <sdevoid@gmail.com>
2012-07-02 19:19:26 -0500

    Initial working list change
    - Supports --with [-w] [attribute] flag for including
      a list of attributes.
    
    - TODO : Implement --verbose [-v] defaults for
      various classes.
    
    - TODO : clean up code, it's a mess.

--- lib/ModelSEED/App/mseed/Command/list.pm
+++ lib/ModelSEED/App/mseed/Command/list.pm
@@ -8,7 +8,15 @@ use Class::Autouse qw(
     ModelSEED::App::Helpers
 );
 use base 'App::Cmd::Command';
-use Data::Dumper;
+
+
+sub abstract { return "List and retrive objects from workspace or datastore."; }
+sub opt_spec {
+    return (
+        ["verbose|v", "Print out additional information about the object, tab-delimited"],
+        ["with|w:s@", "Append a tab-delimited column with this attribute"],
+    );
+}
 
 sub execute {
     my ($self, $opts, $args) = @_;
@@ -25,37 +33,47 @@ sub execute {
         # Base level collection ( want a list of aliases )
         if($ref->type eq 'collection' && 0 == @{$ref->parent_collections}) {
             my $aliases = $store->get_aliases({ type => $ref->base });
-            foreach my $a (@$aliases) {
-                print $a->{type} . "/" . $a->{owner} . "/" . $a->{alias} . "\n";
-            }
+            # Construct references from alias data
+            # TODO: Why isn't this part of Store / Database ?
+            my $refs = [
+                  map { $_->{type} . "/" . $_->{owner} . "/" . $_->{alias} }
+                  @$aliases
+            ];
+            $self->printForReferences($refs, $opts, $store);
         # Subobject listing. want a list of ids under subobject
         } elsif($ref->type eq 'collection' && @{$ref->parent_collections}) {
-            my $data = $store->get_data($ref->parent_objects->[0]);
-            my @sections = split($ref->delimiter, $ref->base);
-            my $subtype = pop @sections;
-            # Ancestor_uuids is just a list of uuids, special case
-            if($subtype eq 'ancestor_uuids') {
-                my $grandparent_ref = ModelSEED::Reference->new(ref => $ref->parent_objects->[0]);
-                foreach my $o (@{$data->{$subtype}}) {
-                    print $grandparent_ref->base . "/" . $o . "\n";
-                }
+            my $need_object = (defined($opts->{with}) || defined($opts->{verbose}));
+            my $refs;
+            if ($need_object) {
+                my $base_object = $store->get_object($ref->parent_objects->[0]);
+                my $subtypes = $ref->base_types;
+                my $subtype  = $subtypes->[@$subtypes - 1];
+                my $data     = $base_object->$subtype;
+                $self->printForData($ref, $data, $opts);
             } else {
-                foreach my $o ( @{$data->{$subtype}} ) {
-                    print $ref->base . "/" . $o->{uuid} . "\n";
+                my $data = $store->get_data($ref->parent_objects->[0]);
+                my $subtypes = $ref->base_types;
+                my $subtype  = $subtypes->[@$subtypes - 1];
+                if ($subtype eq 'ancestor_uuids') {
+                    $refs = [ map { $ref->base . "/" . $_ } @{$data->{$subtype}} ];
+                } else {
+                    $refs = [ map { $ref->base . "/" . $_->{uuid} } @{$data->{$subtype}} ];
                 }
+                print join("\n", @$refs) . "\n";
             }
         # Want a breakdown of the subobjects
         } elsif($ref->type eq 'object') {
             my $data = $store->get_data($ref->base . $ref->delimiter . $ref->id);
+            # Calculate the size of the padding between first and second column
             my $max =  max map { length $_ } keys %$data;
             $max += 5;
+            # Print the formatted results
             foreach my $k ( keys %$data ) {
                 my $v = $data->{$k};
                 if(ref($v) eq 'ARRAY') {
                     printf("%-${max}s(%d)\n", ($k, scalar(@$v)));
                 }
             }
-            # how the fuck
         }
     } else {
         # ref was an empty string or completely invalid 
@@ -74,13 +92,60 @@ sub execute {
     }
 }
 
-sub abstract {
-    return "List and retrive objects from workspace or datastore.";
+sub printForReferences {
+    my ($self, $refstrs, $opts, $store) = @_;
+    my $need_object = (defined($opts->{with}) || defined($opts->{verbose}));
+    my $columns = $self->determineColumns($refstrs->[0], $opts);
+    print join("\t", @$columns) . "\n" if (@$columns > 1);
+    foreach my $refstr (@$refstrs) {
+        my $o;
+        if ($need_object) {
+            $o = $store->get_object($refstr);
+        }
+        print $self->formatOutput($refstr, $o, $columns); 
+    }
 }
 
-sub _processRef {
-    my ($ref) = @_;
-    return split(/\//, $ref);
+sub printForData {
+    my ($self, $ref, $data, $opts) = @_;
+    my $columns = $self->determineColumns($ref->ref, $opts);
+    print join("\t", @$columns) . "\n" if (@$columns > 1);
+    foreach my $o (@$data) {
+        my $refstr = $ref->base . $ref->delimiter . $o->uuid;
+        print $self->formatOutput($refstr, $o, $columns);
+    }
+}
+
+
+sub determineColumns {
+    my ($self, $refstr, $opts) = @_;
+    my $ref = ModelSEED::Reference->new(ref => $refstr);
+    my $types = $ref->base_types;
+    my $type  = $types->[@$types - 1];
+    my $with = [ "Reference" ];
+    push(@$with, @{$opts->{with} || []});
+    if ( $opts->{verbose} ) {
+        # If we asked for verbose, print out some default attributes
+        my $additional_with = $self->verboseRules($type);
+        foreach my $attr (@$additional_with) {
+            # Append attributes unless we've already specified them
+            push(@$with, $attr) unless($attr ~~ @$with);
+        }
+    }
+    return $with;
+}
+
+sub formatOutput {
+    my ($self, $refstr, $object, $columns) = @_;
+    my $with = [ @$columns ];
+    shift @$with; # Remove "Reference" column
+    my $parts = [ $refstr ];
+    foreach my $attr (@$with) {
+        my $value = (ref $object ne 'HASH') ? $object->$attr : $object->{$attr};
+        $value =~ s/\t/\\t/;
+        push(@$parts, $value);
+    }
+    return join("\t", @$parts) . "\n";
 }
 
 1;
