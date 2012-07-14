74c1a8ae1d7e1c99184d2639ac624722fa0c7caf
Paul Frybarger <pfrybarger@gmail.com>
2012-07-14 18:32:41 -0500

    Fixed the ms commands to correctly call mscli.pl even when environment has not yet been set. Removed redundant 'use lib' commands in mscli.pl

--- bin/mscli.pl
+++ bin/mscli.pl
@@ -1,11 +1,6 @@
 #!/usr/bin/env perl
 use FindBin qw($Bin);
 use lib $Bin.'/../config';
-use lib $Bin.'/../lib/PPO';
-use lib $Bin.'/../lib/myRAST';
-use lib $Bin.'/../lib/FigKernelPackages';
-use lib $Bin.'/../lib/ModelSEED/ModelSEEDClients/';
-use lib $Bin.'/../lib';
 use ModelSEEDbootstrap;
 use Class::Autouse qw(
     ModelSEED::App::mseed
